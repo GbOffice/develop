762afbbc0f371fc5013b26767346dbc21cc2866b
Place2025 <2154298633@qq.com>
2025-04-04 21:51:27 +0800

    oo

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 cd build_tools/develop
-docker pull hub.gboffice.com:5000/gboffice/documentserver:latest
+docker pull onlyoffice/documentserver:latest
 docker build --no-cache -t dev-documentserver .
--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
-git clone git@github.com:GbOffice/build_tools.git
+git clone git@github.com:ONLYOFFICE/build_tools.git
 git clone git@github.com:GbOffice/web-apps.git
 git clone git@github.com:GbOffice/sdkjs.git
